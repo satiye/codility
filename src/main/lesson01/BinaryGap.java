333a94a65828ddfd1d04b01f6b29ebaf2b92e5bb
Satiye Çatal <satiyecatal@gmail.com>
2021-01-25 00:23:36 +0300

    description added

--- src/main/lesson01/BinaryGap.java
+++ src/main/lesson01/BinaryGap.java
@@ -1,5 +1,16 @@
 package main.lesson01;
 
+/*
+  When converting the number N to its binary representation, it should be tracked to see if 1 has been seen before.
+
+  If 1 is seen in the conversion:
+  	- If there is 1 seen before, it means a binary gap has been created. Its length should be checked. And the O counter must be reset.
+  	- If 1 was not seen before, 1 should be set as seen.
+  If 0 is seen in the conversion:
+    - If 1 is seen before, 0 count should be incremented. 
+    - If 1 was not seen before, there is no need to do anything.
+ */
+
 public class BinaryGap {
 	public int solution(int N) {
         int maxBinaryGapLength = 0;
